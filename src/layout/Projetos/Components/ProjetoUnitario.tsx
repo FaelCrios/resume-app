d8439444d820a3a458d5939438329f383c261ded
Rafael Colin <faelnek@gmail.com>
2023-05-23 15:54:23 -0300

    Projects page initialized

---
+++ src/layout/Projetos/Components/ProjetoUnitario.tsx
@@ -0,0 +1,28 @@
+export const ProjetoUnitario: React.FC<{
+  url: string;
+  img: any;
+  titulo: string;
+  paragrafo: any;
+  width: any;
+}> = (props) => {
+  return (
+    <div className="col-md-6">
+    <div className="h-100 p-5 bg-body-tertiary border rounded-3">
+      <a
+        className=""
+        href={props.url}
+      >
+        <img
+          src={require(`../../../Images/PublicImages/${props.img}`)}
+          onClick={() => {}}
+          width={props.width} 
+        />
+      </a>
+      <h2 className="fw-normal">{props.titulo}</h2>
+      <p>
+        {props.paragrafo}
+      </p>
+    </div>
+    </div>
+  );
+};
