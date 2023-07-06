80378076d63495aaaa2f0dfa2cff13be4c254a5f
Rafael Colin <faelnek@gmail.com>
2023-07-06 11:48:10 -0300

    Navbar fixed on TOP

--- src/layout/NavBarAndFooter/Navbar.tsx
+++ src/layout/NavBarAndFooter/Navbar.tsx
@@ -2,10 +2,10 @@ import { NavLink } from "react-router-dom";
 
 export const Navbar = () => {
   return (
-    <div className="text-bg-dark  py-3 p-3">
-    <nav className=" container text-center ">
+    <div className="py-5 p-3 text-center">
+    <nav className="bg-dark text-center fixed-top">
       <div className="">
-        <div className="d-flex justify-content-evenly px-3 py-3 fo">
+        <div className="d-flex justify-content-evenly px-3 py-3 ">
           <div>
             <NavLink className="nav-link text-light fs-2" to="/home">
               Home
